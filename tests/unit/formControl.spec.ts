340956f10a14d04a0c2d7b81222beb1b0cfa203c
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2021-10-09 23:51:06 +0800

    test(from group): add valid  & nest test

--- tests/unit/formControl.spec.ts
+++ tests/unit/formControl.spec.ts
@@ -1,4 +1,4 @@
-import { FormControl } from '../../src'
+import { FormControl, requiredValidator } from '../../src'
 
 describe('FormControl', () => {
   it('FormControl can be created with default values', () => {
@@ -11,5 +11,47 @@ describe('FormControl', () => {
     const testFormControl = new FormControl<string>({ defaultValue: 'test' })
 
     expect(testFormControl.ref.value).toMatch('test')
+  })
+})
+
+describe('FormControl Validator', () => {
+  it('FormControl property dirty should be false by default', () => {
+    const testFormControl = new FormControl<string>({
+      defaultValue: '',
+      validators: [requiredValidator()],
+    })
+
+    expect(testFormControl.dirty.value).toBe(false)
+  })
+
+  it('FormControl property valid should be false by default', () => {
+    const testFormControl = new FormControl<string>({
+      defaultValue: '',
+      validators: [requiredValidator()],
+    })
+
+    expect(testFormControl.valid.value).toBe(false)
+  })
+
+  it('FormControl property dirty should be true after control value change', () => {
+    const testFormControl = new FormControl<string>({
+      defaultValue: '',
+      validators: [requiredValidator()],
+    })
+
+    testFormControl.ref.value = 'test'
+
+    expect(testFormControl.dirty.value).toBe(true)
+  })
+
+  it('FormControl property valid should be true after control value change', () => {
+    const testFormControl = new FormControl<string>({
+      defaultValue: '',
+      validators: [requiredValidator()],
+    })
+
+    testFormControl.ref.value = 'test'
+
+    expect(testFormControl.valid.value).toBe(true)
   })
 })
--- tests/unit/formGroup.spec.ts
+++ tests/unit/formGroup.spec.ts
@@ -1,4 +1,4 @@
-import FormGroup, { FormControl } from '../../src'
+import FormGroup, { FormControl, requiredValidator } from '../../src'
 
 describe('Formgroup', () => {
   const testFormGroup = new FormGroup<{ test: string }>({
@@ -9,7 +9,51 @@ describe('Formgroup', () => {
     expect(testFormGroup.values.value.test).toMatch('test')
   })
 
-  it('controls of Formgroup should be FormControl', () => {
+  it('Controls of Formgroup should be FormControl', () => {
     expect(testFormGroup.controls.test).toBeInstanceOf(FormControl)
+  })
+})
+
+describe('Nested Formgroup', () => {
+  const testFormGroup = new FormGroup<{ test: { testChild: string } }>({
+    test: { testChild: 'test' },
+  })
+
+  it('Nested Formgroup can be created', () => {
+    expect(testFormGroup.values.value.test.testChild).toMatch('test')
+  })
+})
+
+describe('Form Validator', () => {
+  it('Formgroup property dirty should be false by default', () => {
+    const testFormGroup = new FormGroup<{ test: string }>({
+      test: { defaultValue: '', validators: [requiredValidator()] },
+    })
+    expect(testFormGroup.dirty.value).toBe(false)
+  })
+
+  it('Formgroup property valid should be false by default', () => {
+    const testFormGroup = new FormGroup<{ test: string }>({
+      test: { defaultValue: '', validators: [requiredValidator()] },
+    })
+    console.log('testFormGroup.valid.value', testFormGroup.valid.value)
+
+    expect(testFormGroup.valid.value).toBe(false)
+  })
+
+  it('Formgroup property dirty should be true after control value change', () => {
+    const testFormGroup = new FormGroup<{ test: string }>({
+      test: { defaultValue: '', validators: [requiredValidator()] },
+    })
+    testFormGroup.refs.test.value = 'test'
+    expect(testFormGroup.dirty.value).toBe(true)
+  })
+
+  it('Formgroup property valid should be true after control value change', () => {
+    const testFormGroup = new FormGroup<{ test: string }>({
+      test: { defaultValue: '', validators: [requiredValidator()] },
+    })
+    testFormGroup.refs.test.value = 'test'
+    expect(testFormGroup.valid.value).toBe(true)
   })
 })
